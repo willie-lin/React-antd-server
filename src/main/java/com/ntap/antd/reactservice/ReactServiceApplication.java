00e5d2a58076c7a4dbb74225dfae97f5502e1db4
Willie-lin <yyhmmwan@gmail.com>
2018-11-20 15:32:16 +0800

    Initial commit

---
+++ src/main/java/com/ntap/antd/reactservice/ReactServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ntap.antd.reactservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactServiceApplication.class, args);
+    }
+}
