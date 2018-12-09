14a5748311906fb28b864df4cb29736248c0c798
Willie-lin <yyhmmwan@gmail.com>
2018-12-09 16:31:25 +0800

    add Voter

---
+++ src/main/java/com/ntap/antd/reactservice/models/Voter.java
@@ -0,0 +1,15 @@
+package com.ntap.antd.reactservice.models;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Package com.ntap.antd.reactservice.models
+ * @auther YuAn
+ * @Date 2018/11/28
+ * @Time 15:59
+ * @Project_name React-antd-server
+ * To change this template use File | Settings | File Templates.
+ * @Description ${DESCRIPTION}
+ */
+public class Voter {
+}
