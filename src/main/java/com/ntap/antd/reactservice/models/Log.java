873aebeaa5a1b21d514d438854a068bb8f0203eb
Willie-lin <yyhmmwan@gmail.com>
2018-12-17 22:01:31 +0800

    add Voter

---
+++ src/main/java/com/ntap/antd/reactservice/models/Log.java
@@ -0,0 +1,15 @@
+package com.ntap.antd.reactservice.models;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Package com.ntap.antd.reactservice.models
+ * @auther YuAn
+ * @Date 2018/12/11
+ * @Time 18:14
+ * @Project_name React-antd-server
+ * To change this template use File | Settings | File Templates.
+ * @Description ${DESCRIPTION}
+ */
+public class Log {
+}
